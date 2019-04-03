15bd7399942b8a6ddc605e14e6c5601c1f8ef977
zgl <285683264@qq.com>
2019-04-03 15:59:57 +0800

    add missing files

--- springboot-configuration/src/main/java/com/zgl/springboot/configuration/DataSourceConfiguration.java
+++ springboot-configuration/src/main/java/com/zgl/springboot/configuration/DataSourceConfiguration.java
@@ -3,12 +3,10 @@ package com.zgl.springboot.configuration;
 import com.alibaba.druid.pool.DruidDataSource;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.Resource;
 import java.sql.SQLException;
 
 /**
